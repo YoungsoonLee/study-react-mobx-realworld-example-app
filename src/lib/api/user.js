7f5434a66fbd73a3696f4e3d93731da2b21ce3f1
JayJay <youngtip@gmail.com>
2019-06-19 15:43:31 +0900

    .

--- src/lib/api/user.js
+++ src/lib/api/user.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 import {BACKEND_API} from '../../utils/constants';
 
 // for indonesia
-export const registerEmailIndonesia = (email, ip) => axios.post('https://127.0.0.1:10443'+'/v1/user/registerIndonesia',{email,ip});
+export const registerEmailIndonesia = (email, ip) => axios.post('https://15.164.35.71:10443'+'/v1/user/registerIndonesia',{email,ip});

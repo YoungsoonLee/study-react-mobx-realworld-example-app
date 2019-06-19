f09632c5faf1971ca9d72c89fce09d0fb0c6dc8f
JayJay <youngtip@gmail.com>
2019-06-19 12:07:25 +0900

    .

--- src/lib/api/user.js
+++ src/lib/api/user.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 import {BACKEND_API} from '../../utils/constants';
 
 // for indonesia
-export const registerEmailIndonesia = (email, ip) => axios.post('http://closeindi.naddic.com:3000'+'/v1/user/registerIndonesia',{email,ip});
+export const registerEmailIndonesia = (email, ip) => axios.post('https://closeindi.naddic.com:3000'+'/v1/user/registerIndonesia',{email,ip});
